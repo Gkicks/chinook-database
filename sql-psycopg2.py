9c9fe93912e59b2fde44e1b315872bbc25b75161
Gkicks <>
2023-11-25 14:48:22 +0000

    First Look At An RDBMS

---
+++ sql-psycopg2.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the results (single)
+# results = cursor.fetchone()
